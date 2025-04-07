2648e5c1971eb4514236385de3e43de73778a238
Mazher <mazheriftikhar1@gmail.com>
2025-04-07 23:21:51 +0500

    7-Apr

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translate(0%)" },
+          "95%": { transform: "translate(-95%)" }
+        },
+        scrollReverse: {
+          "0%": { transform: "translate(-55%)" },
+          "95%": { transform: "translate(55%)" }
+        }
+
+      },
+      animation: {
+        scroll: 'scroll 100s infinite',
+        scrollReverse: 'scrollReverse 100s infinite',
+      }
+    }
   },
   plugins: [],
 }
