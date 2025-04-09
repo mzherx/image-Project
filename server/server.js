6c0414791a4392fc6dbb3e24f29261a268b9fe8b
Mazher <mazheriftikhar1@gmail.com>
2025-04-09 19:33:42 +0500

    9-Apr

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://imagify-frontend.vercel.app'],
+    origin: ['http://localhost:5173', 'https://imagify-ai-flame.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'token'], // Include your custom headers
     credentials: true
