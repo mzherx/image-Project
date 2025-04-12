c837e8169e8d018f8c9cb004a99998a7578e97ae
Mazher <mazheriftikhar1@gmail.com>
2025-04-12 16:23:49 +0500

    12-Apr

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html')
-    }
+    emptyOutDir: true
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  base: './'
+  base: ''
 });
