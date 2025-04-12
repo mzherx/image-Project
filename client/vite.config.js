e0f6bc60f0d7aa3c38c22d2b48b361dcd2a93d0e
Mazher <mazheriftikhar1@gmail.com>
2025-04-12 16:18:41 +0500

    12-Apr

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  base: '/'
 });
